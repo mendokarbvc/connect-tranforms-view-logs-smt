11a5a335ea7613565de4bd17acf9e709fa1abfac
oscar mendoza <oscar.mendoza.se@nuamx.com>
2024-08-02 11:22:15 -0500

    versionamiento de transformacion para ver logs de schema

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # connect-tranforms-view-logs-smt
 SMT transformation to view logs of records
+
+
+Proyecto de Ejemplo con Kafka Connect y Transformación "ViewLogs"
+
+Este proyecto es un ejemplo de cómo configurar y compilar un proyecto Maven que utiliza Kafka Connect con una transformación personalizada llamada "ViewLogs".
+
+Requisitos previos
+Antes de comenzar, asegúrate de tener instalados los siguientes requisitos:
+
+Java Development Kit (JDK) 8 o superior
+Apache Maven 3.6.0 o superior
+Apache Kafka
+Estructura del proyecto
+La estructura del proyecto es la siguiente:
+
+├── pom.xml
+├── src
+│   ├── main
+│   │   ├── java
+│   │   │   └── com
+│   │   │       └── nuamx
+│   │   │           └── temp
+│   │   │               └── kafka
+│   │   │                   └── connect
+│   │   │                       └── smt
+│   │   │                           └── ViewLogs.java
+│   │   └── resources
+│   │       └── log4j.properties
+│   └── test
+│       └── java
+│           └── com
+│               └── nuamx
+│                   └── temp
+│                       └── kafka
+│                           └── connect
+│                               └── smt
+│                                   └── ViewLogsTest.java
+└── README.md
+
+Compilación del proyecto
+Para compilar el proyecto, sigue estos pasos:
+
+Clona el repositorio o descarga el código fuente.
+
+Navega al directorio raíz del proyecto.
+
+Ejecuta el siguiente comando para compilar el proyecto utilizando Maven:
+
+mvn clean package
+
+Esto generará un archivo JAR en el directorio target.
+
+Configuración de Kafka Connect
+Para utilizar la transformación "ViewLogs" en Kafka Connect, debes configurar tu conector. Aquí tienes un ejemplo de configuración en formato JSON:
+
+{
+  "name": "mi-conector",
+  "config": {
+    "connector.class": "org.apache.kafka.connect.file.FileStreamSourceConnector",
+    "tasks.max": "1",
+    "file": "/path/to/input/file.txt",
+    "topic": "mi-topic",
+    "transforms": "ViewLogs",
+    "transforms.ViewLogs.type": "com.nuamx.temp.kafka.connect.smt.ViewLogs$Value"
+  }
+}
---
+++ src/main/java/com/nuamx/temp/kafka/connect/smt/ViewLogs.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright © 2019 Christopher Matta (chris.matta@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nuamx.temp.kafka.connect.smt;
+
+import org.apache.kafka.common.cache.Cache;
+import org.apache.kafka.common.cache.LRUCache;
+import org.apache.kafka.common.cache.SynchronizedCache;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.ConnectRecord;
+import org.apache.kafka.connect.data.Field;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
+
+import org.apache.kafka.connect.transforms.Transformation;
+import org.apache.kafka.connect.transforms.util.SchemaUtil;
+import org.apache.kafka.connect.transforms.util.SimpleConfig;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.apache.kafka.connect.transforms.util.Requirements.requireMap;
+import static org.apache.kafka.connect.transforms.util.Requirements.requireStruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class ViewLogs<R extends ConnectRecord<R>> implements Transformation<R> {
+
+  private static final Logger logger = LoggerFactory.getLogger(ViewLogs.class);
+
+  public static final String OVERVIEW_DOC =
+    "Insert a random UUID into a connect record";
+
+  private interface ConfigName {
+  }
+
+  public static final ConfigDef CONFIG_DEF = new ConfigDef();
+
+  private static final String PURPOSE = "adding UUID to record";
+
+  private String fieldName;
+
+  private Cache<Schema, Schema> schemaUpdateCache;
+
+  @Override
+  public void configure(Map<String, ?> props) {
+    final SimpleConfig config = new SimpleConfig(CONFIG_DEF, props);
+    fieldName = "";//config.getString(ConfigName.UUID_FIELD_NAME);
+
+    schemaUpdateCache = new SynchronizedCache<>(new LRUCache<Schema, Schema>(16));
+  }
+
+
+  @Override
+  public R apply(R record) {
+    System.out.println("MENSAJERECORD");
+    System.err.println(record);
+
+
+    logger.debug("MENSAJE applySchemaless");
+    logger.debug("Record: {}", record);
+
+    if (operatingSchema(record) == null) {
+      return applySchemaless(record);
+    } else {
+      return applyWithSchema(record);
+    }
+  }
+
+  private R applySchemaless(R record) {
+
+    System.out.println("MENSAJE applySchemaless");
+    System.err.println(record);
+
+    return record;
+  }
+
+  private R applyWithSchema(R record) {
+
+    System.out.println("MENSAJE applyWithSchema");
+    System.err.println(record);
+
+    return record;
+  }
+
+  @Override
+  public ConfigDef config() {
+    return CONFIG_DEF;
+  }
+
+  @Override
+  public void close() {
+    schemaUpdateCache = null;
+  }
+
+  protected abstract Schema operatingSchema(R record);
+
+  protected abstract Object operatingValue(R record);
+
+  protected abstract R newRecord(R record, Schema updatedSchema, Object updatedValue);
+
+  public static class Key<R extends ConnectRecord<R>> extends ViewLogs<R> {
+
+    @Override
+    protected Schema operatingSchema(R record) {
+      return record.keySchema();
+    }
+
+    @Override
+    protected Object operatingValue(R record) {
+      return record.key();
+    }
+
+    @Override
+    protected R newRecord(R record, Schema updatedSchema, Object updatedValue) {
+      return record.newRecord(record.topic(), record.kafkaPartition(), updatedSchema, updatedValue, record.valueSchema(), record.value(), record.timestamp());
+    }
+
+  }
+
+  public static class Value<R extends ConnectRecord<R>> extends ViewLogs<R> {
+
+    @Override
+    protected Schema operatingSchema(R record) {
+      return record.valueSchema();
+    }
+
+    @Override
+    protected Object operatingValue(R record) {
+      return record.value();
+    }
+
+    @Override
+    protected R newRecord(R record, Schema updatedSchema, Object updatedValue) {
+      return record.newRecord(record.topic(), record.kafkaPartition(), record.keySchema(), record.key(), updatedSchema, updatedValue, record.timestamp());
+    }
+
+  }
+}
+
+
